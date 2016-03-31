1570b88f46a0cd93ef4bebd12e3bd8e9c6316c4d
Abedzadeh5928 <Abed59285928@gmail.com>
2016-03-31 17:47:37 +0430

    Create hack1.lua

---
+++ plugins/hack1.lua
@@ -0,0 +1,12 @@
+Enter file contents heredo
+
+function run(msg, matches)
+  return "تا ساعاتی دیگر هک میشود قربان🙂🙃"
+  end
+return {
+  patterns = {
+    "^هک در چه وضعیتیه ربات",
+  },
+  run = run
+}
+end
