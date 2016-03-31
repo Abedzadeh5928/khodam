e87870f8c8c41d22ddafb1c6940dc57ab088d369
Abedzadeh5928 <Abed59285928@gmail.com>
2016-03-31 17:51:33 +0430

    Create ali.lua

---
+++ plugins/ali.lua
@@ -0,0 +1,13 @@
+Enter file contents here
+do
+
+function run(msg, matches)
+  return "با باباییم چیکار داری ؟؟ 🙂🙃"
+  end
+return {
+  patterns = {
+    "^[Aa][Ll][Ii][Rr][Ee][Zz][Aa]",
+  },
+  run = run
+}
+end
